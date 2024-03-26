649c24a74483da693ecc82f1da35c9739fc08131
정종두 <jongdoo2001@naver.com>
2024-03-26 13:16:10 +0900

    Fix: Batch 중복 허용

--- src/main/java/com/example/springbatchtest/HelloJobConfiguration.java
+++ src/main/java/com/example/springbatchtest/HelloJobConfiguration.java
@@ -1,46 +1,60 @@
 package com.example.springbatchtest;
 
+import com.example.springbatchtest.entity.Member;
+import com.example.springbatchtest.repository.LoginRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.util.List;
+
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class HelloJobConfiguration { // Job 정의
+
 
     @Bean
     public Job simpleJob(JobRepository jobRepository, Step simpleStep1, Step simpleStep2) {
-        return new JobBuilder("simpleJob", jobRepository)
+        return new JobBuilder("simpleJob4", jobRepository)
                 .start(simpleStep1)
                 .next(simpleStep2)
                 .build();
     }
+
     @Bean
-    public Step simpleStep1(JobRepository jobRepository, Tasklet testTasklet, PlatformTransactionManager platformTransactionManager){
-        return new StepBuilder("simpleStep1", jobRepository)
+    public Step simpleStep1(JobRepository jobRepository, Tasklet testTasklet, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("simpleStep3", jobRepository)
+                .allowStartIfComplete(true)
                 .tasklet(testTasklet, platformTransactionManager).build();
     }
 
     @Bean
-    public Step simpleStep2(JobRepository jobRepository, Tasklet testTasklet, PlatformTransactionManager platformTransactionManager){
-        return new StepBuilder("simpleStep2", jobRepository)
+    public Step simpleStep2(JobRepository jobRepository, Tasklet testTasklet, PlatformTransactionManager platformTransactionManager) {
+        return new StepBuilder("simpleStep4", jobRepository)
+                .allowStartIfComplete(true)
                 .tasklet(testTasklet, platformTransactionManager).build();
     }
+
     @Bean
-    public Tasklet testTasklet(){
+    public Tasklet testTasklet() {
         return ((contribution, chunkContext) -> {
             log.info(">>>>> This is Step1");
             return RepeatStatus.FINISHED;
         });
     }
+
+
 
 }
--- src/main/java/com/example/springbatchtest/JobLauncherController.java
+++ src/main/java/com/example/springbatchtest/JobLauncherController.java
@@ -1,14 +1,11 @@
 package com.example.springbatchtest;
 
-import com.example.springbatchtest.entity.Member;
-import com.example.springbatchtest.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,30 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class JobLauncherController {
 
-    private final MemberService memberService;
+
     private final JobLauncher jobLauncher;
+    private final Job job;
 
-    @Autowired
-    Job job;
-
-//    @GetMapping("/test")
-//    public String handle() throws Exception{
-//        jobLauncher.run(job, new JobParameters());
-//
-//        return "test";
-//    }
-
-    @GetMapping("/save")
-    public String saveMember(){
-        memberService.saveMember();
-        return "정상";
+    @Scheduled(cron = "0/10 * * * * *")
+    public void handle() throws Exception{
+        jobLauncher.run(job, new JobParameters());
     }
 
-    @GetMapping("/find")
-    public String findMember(){
-        log.info("값 = {}", memberService.findMember());
-        return "정상";
-    }
+
 
 
 }
